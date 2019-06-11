6e3cc66ad822fbf3d11518fca5074842d8444a3b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-11 01:09:00 +0100

    Cell component inline style

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 class Cell extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      letter: props.letter
-    }
-  }
   render() {
     return (
-      <div>{this.state.letter}</div>
+      <div style={cellStyle}>{this.props.letter.charAt(0)}</div>
     )
   }
 }
 
 Cell.defaultProps = {
   letter: 'T'
+}
+
+const cellStyle = {
+  background: '#345',
+  display: 'inline-block',
+  width: '3rem',
+  height: '3rem',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  margin: '.2rem',
+  padding: '.8rem',
+  fontSize: '2rem',
+  fontWeight: '500',
 }
 
 export default Cell
