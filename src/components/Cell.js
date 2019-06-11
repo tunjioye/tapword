187547d0cea3f58c49f01ee974c659df3fcde214
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-11 07:25:57 +0100

    added style for Cell Component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,29 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 class Cell extends React.Component {
   render() {
     return (
-      <div style={cellStyle}>{this.props.letter.charAt(0)}</div>
-    )
+      <div className="cell">{this.props.letter.charAt(0)}</div>
+    );
   }
 }
 
 Cell.defaultProps = {
   letter: 'T'
-}
-
-const cellStyle = {
-  background: '#345',
-  display: 'inline-block',
-  width: '3rem',
-  height: '3rem',
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  margin: '.2rem',
-  padding: '.8rem',
-  fontSize: '2rem',
-  fontWeight: '500',
-  textTransform: 'uppercase',
 }
 
 export default Cell
