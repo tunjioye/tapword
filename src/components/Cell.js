3ff6666a299ff7398101fde277a104511bd6b4c9
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-11 07:27:29 +0100

    toggle selected style on Cell Component Click

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class Cell extends React.Component {
+  handleClick(e) {
+    let cell = e.target
+    console.log(cell.innerHTML)
+    cell.classList.toggle('selected')
+  }
   render() {
     return (
-      <div className="cell">{this.props.letter.charAt(0)}</div>
+      <div className="cell" onClick={this.handleClick.bind(this)}>{this.props.letter.charAt(0)}</div>
     );
   }
 }
