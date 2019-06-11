f00b405670e5fc3a5812dbb13bc6028445c03d5b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-11 01:40:42 +0100

    created component Grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Row from './components/Row';
+import Grid from './components/Grid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Row />
+        <Grid />
       </header>
     </div>
   );
---
+++ src/components/Grid.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Row from './Row';
+
+class Grid extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      size: props.size,
+      rows: props.rows
+    }
+    this.randomLetters = this.randomLetters.bind(this)
+  }
+  randomLetters() {
+    const chars = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+    let letters = []
+    for (let i = 0; i < this.state.size; i++) {
+      letters[i] = [...Array(parseInt(this.state.size))].map(x => chars[Math.random() * chars.length | 0]).join('')
+    }
+    return letters
+  }
+  componentDidMount() {
+    if (this.props.generate) {
+      this.setState({
+        rows: this.randomLetters()
+      })
+    }
+  }
+  render() {
+    let rows = []
+    for (let i = 0; i < this.state.size; i++) {
+      rows[i] = <Row key={i.toString()} size={this.state.size} letters={this.state.rows[i]} />
+    }
+    return (
+      <div>
+        {rows}
+      </div>
+    );
+  }
+}
+
+Grid.defaultProps = {
+  generate: true,
+  size: 5,
+  rows: [
+    'TAP',
+    'WORD',
+    'GAME',
+    'BY',
+    'TUNJI',
+  ]
+}
+
+export default Grid;
