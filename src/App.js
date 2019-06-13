18c145ccf601e6911b57caa58aaca96866d93334
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-14 00:02:34 +0100

    spellcheck quick test with logging to console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
 import Grid from './components/Grid';
+import spellcheck from './spellcheck';
+
+spellcheck.get('?text=tap')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    error = error.toJSON();
+    console.log((error.description) ? (error.message + ':', error.description) : error.message);
+  });
 
 function App() {
   return (
