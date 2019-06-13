de30ae30543c23c83505554f5ebf59c58a7cf57d
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-14 00:03:36 +0100

    removed spellcheck quick test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './App.css';
 import Grid from './components/Grid';
-import spellcheck from './spellcheck';
-
-spellcheck.get('?text=tap')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    error = error.toJSON();
-    console.log((error.description) ? (error.message + ':', error.description) : error.message);
-  });
 
 function App() {
   return (
