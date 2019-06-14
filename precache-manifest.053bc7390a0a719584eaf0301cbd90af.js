8b32a45961fa072e58b13cdcf810e976cf4de78e
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-14 15:15:01 +0100

    Updates

---
+++ precache-manifest.053bc7390a0a719584eaf0301cbd90af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dadf52cd28a11af7994256680dc81f",
+    "url": "/tapword/index.html"
+  },
+  {
+    "revision": "7e1b1dc0a5c37e47d8bd",
+    "url": "/tapword/static/css/main.5491a6aa.chunk.css"
+  },
+  {
+    "revision": "1cbfbecdaf0ec4755f4c",
+    "url": "/tapword/static/js/2.b9c8a2f1.chunk.js"
+  },
+  {
+    "revision": "7e1b1dc0a5c37e47d8bd",
+    "url": "/tapword/static/js/main.99ea7e11.chunk.js"
+  },
+  {
+    "revision": "7cc087fe62f8accee4dc",
+    "url": "/tapword/static/js/runtime~main.d3b60f95.js"
+  }
+]);
