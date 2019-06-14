fd7c520dd4947f886e43bfb1e365646bbf16a299
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-14 17:41:06 +0100

    added tooltip to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,26 +4,26 @@ class Sidebar extends React.Component {
   render() {
     let timer;
     if (this.props.endless) {
-      timer = <div className="button alt">&infin;</div>;
+      timer = <div className="button alt tooltip" data-tooltip="Timer">&infin;</div>;
     } else {
-      timer = <div className="button alt">{this.props.minutes}<span className="highlight">m</span></div>;
+      timer = <div className="button alt tooltip" data-tooltip="Timer">{this.props.minutes}<span className="highlight">m</span></div>;
     }
     return (
       <div className="sidebar">
         <div className="cell">
-          <div className="button alt"><span className="highlight">x</span><span id="multiplier">{this.props.multiplier}</span></div>
+          <div className="button alt tooltip" data-tooltip="Score Multiplier"><span className="highlight">x</span><span id="multiplier">{this.props.multiplier}</span></div>
         </div>
         <div className="cell">
           {timer}
         </div>
         <div className="cell">
-          <div className="button alt">&#8862;<sub className="highlight">{this.props.size}</sub></div>
+          <div className="button alt tooltip" data-tooltip="Grid Size">&#8862;<sub className="highlight">{this.props.size}</sub></div>
         </div>
         <div className="cell">
-          <button id="undo" className="button" onClick={this.props.handleUndoButtonClick}>&#8630;</button>
+          <button id="undo" className="button tooltip" onClick={this.props.handleUndoButtonClick} data-tooltip="Undo Word">&#8630;</button>
         </div>
         <div className="cell">
-          <button id="submit" className="button" onClick={this.props.handleSubmitButtonClick}>&#8629;</button>
+          <button id="submit" className="button tooltip" onClick={this.props.handleSubmitButtonClick} data-tooltip="Submit Word">&#8629;</button>
         </div>
       </div>
     );
