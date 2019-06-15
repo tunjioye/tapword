941a26570457f701e8dcf3b96696255ab610aa07
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-15 19:49:13 +0100

    created component Help

---
+++ src/components/Help.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Help() {
+  return (
+    <div className="modal" id="help">
+      <a href="#close" className="modal-overlay" aria-label="Close"> </a>
+      <div className="modal-container">
+        <div className="modal-header">
+          <a href="#close" className="btn btn-clear" aria-label="Close"> </a>
+          <div className="modal-title">
+            <strong style={spacedTypo}>HELP &amp; About</strong>
+          </div>
+        </div>
+        <div className="modal-body">
+          <div className="content">
+            <div>...</div>
+          </div>
+        </div>
+        <div className="modal-footer">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const spacedTypo = {
+  letterSpacing: '0.1rem',
+  wordSpacing: '0.2rem',
+  fontWeight: 500
+}
+
+export default Help;
