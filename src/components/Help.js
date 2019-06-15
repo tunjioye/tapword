30d533de652b70ef58996d59dcdfbb922e5ec8cf
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-15 23:26:47 +0100

    added content into help modal

--- src/components/Help.js
+++ src/components/Help.js
@@ -13,10 +13,40 @@ function Help() {
         </div>
         <div className="modal-body">
           <div className="content">
-            <div>...</div>
+            <div>
+              <h5 style={spacedTypo}>HOW TO PLAY</h5>
+              <div>...</div>
+              <div>&nbsp;</div>
+              <div>&bull; Select some letters to make a word</div>
+              <div>&bull; Click Submit Button to evaluate you Score</div>
+              <div>&bull; QUITING IS NOT AN OPTION!</div>
+            </div>
+            <br />
+            <br />
+            <div>
+              <h5 style={spacedTypo}>KEYBOARD SHORTCUTS</h5>
+              <div>...</div>
+              <div>&nbsp;</div>
+              <div><kbd style={kbd}>BKSP</kbd> UNDO</div>
+              <div>&nbsp;</div>
+              <div><kbd style={kbd}>ENTER</kbd> SUBMIT</div>
+            </div>
+            <br />
+            <br />
+            <div>
+              <h5 style={spacedTypo}>ABOUT</h5>
+              <div>...</div>
+              <div>&nbsp;</div>
+              <div>Tapword - <em>words are limitless</em> : is a word game that tests your knowledge of English words. Tapword provides a means to combine as many letters as you can to make the longest word you possibly can.</div>
+              <div>&nbsp;</div>
+              <div style={textCenter}><em>Play and Boast of your Vocabulary among friends</em></div>
+              <div>&nbsp;</div>
+              <div style={{ ...spacedTypo, ...textCenter }}>+++ THANK YOU +++</div>
+            </div>
           </div>
         </div>
         <div className="modal-footer">
+          <div style={{ ...spacedTypo, ...textCenter }}>*** <a style={customLink} href="https://github.com/tunjioye/tapword" target="_blank" rel="noopener noreferrer">Git Repo</a> ***</div>
         </div>
       </div>
     </div>
@@ -27,7 +57,22 @@ const spacedTypo = {
   letterSpacing: '0.1rem',
   wordSpacing: '0.2rem',
   textTransform: 'uppercase',
+  margin: 0,
   fontWeight: 500
+}
+
+const kbd = {
+  border: '1px solid #28F',
+  padding: '3px 4px 3px 6px'
+}
+
+const textCenter = {
+  textAlign: 'center'
+}
+
+const customLink = {
+  color: '#FFF',
+  textDecoration: 'none'
 }
 
 export default Help;
