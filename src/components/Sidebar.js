1f29fbf213f955a9c389a550cc01e304e646de6b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-15 20:20:32 +0100

    moved tooltip to elements parent

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,38 +13,38 @@ class Sidebar extends React.Component {
   render() {
     let timer;
     if (this.props.endless) {
-      timer = <div className="button alt tooltip" data-tooltip="Timer">&infin;</div>;
+      timer = <div className="button alt">&infin;</div>;
     } else {
-      timer = <div className="button alt tooltip" data-tooltip="Timer">{this.props.minutes}<span className="highlight">m</span></div>;
+      timer = <div className="button alt">{this.props.minutes}<span className="highlight">m</span></div>;
     }
     return (
       <div className="sidebar">
         <div className="sidebar-inner">
-          <div className="cell">
-            <div className="button alt tooltip" data-tooltip="Score Multiplier"><span className="highlight">x</span><span id="multiplier">{this.props.multiplier}</span></div>
+          <div className="cell tooltip" data-tooltip="Score Multiplier">
+            <div className="button alt"><span className="highlight">x</span><span id="multiplier">{this.props.multiplier}</span></div>
           </div>
-          <div className="cell">
+          <div className="cell tooltip" data-tooltip="Timer">
             {timer}
           </div>
-          <div className="cell">
-            <div className="button alt tooltip" data-tooltip="Grid Size">&#8862;<sub className="highlight">{this.props.size}</sub></div>
+          <div className="cell tooltip" data-tooltip="Grid Size">
+            <div className="button alt">&#8862;<sub className="highlight">{this.props.size}</sub></div>
           </div>
-          <div className="cell">
-            <button id="undo" className="button tooltip" onClick={this.props.handleUndoButtonClick} data-tooltip="Undo Word">&#8630;</button>
+          <div className="cell tooltip" data-tooltip="Undo Word">
+            <button id="undo" className="button" onClick={this.props.handleUndoButtonClick}>&#8630;</button>
           </div>
-          <div className="cell">
-            <button id="submit" className="button tooltip" onClick={this.props.handleSubmitButtonClick} data-tooltip="Submit Word">&#8629;</button>
+          <div className="cell tooltip" data-tooltip="Submit Word">
+            <button id="submit" className="button" onClick={this.props.handleSubmitButtonClick}>&#8629;</button>
           </div>
         </div>
         <div className="sidebar-inner">
-          <div className="cell">
-            <div className="button alt tooltip" data-tooltip="Settings" onClick={this.openSettingsModal.bind(this)}>&#10033;</div>
+          <div className="cell tooltip" data-tooltip="Settings">
+            <div className="button alt" onClick={this.openSettingsModal.bind(this)}>&#10033;</div>
           </div>
-          <div className="cell">
-            <div className="button alt tooltip" data-tooltip="Help" onClick={this.openHelpModal.bind(this)}>&#63;</div>
+          <div className="cell tooltip" data-tooltip="Help">
+            <div className="button alt" onClick={this.openHelpModal.bind(this)}>&#63;</div>
           </div>
-          <div className="cell">
-            <button id="quit" className="button tooltip" data-tooltip="Quit" onClick={this.handleQuit.bind(this)}>&times;</button>
+          <div className="cell tooltip" data-tooltip="Quit">
+            <button id="quit" className="button" onClick={this.handleQuit.bind(this)}>&times;</button>
           </div>
         </div>
       </div>
