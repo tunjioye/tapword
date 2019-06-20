a7d11a8f3bbd53837432fdba6b758b23aa57672c
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-20 19:15:20 +0100

    Added Topbar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Topbar from './components/Topbar';
 import Grid from './components/Grid';
 import Settings from './components/Settings';
 import Help from './components/Help';
@@ -8,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{ display: 'block' }}>
+          <Topbar />
+        </div>
         <Grid cellToggle={true} shuffle={true} shuffleAll={true} endless={true} />
       </header>
       <Settings />
