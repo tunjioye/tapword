6ba91e7550b5f7cfcda74c09a66b9cfc7cd6247d
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-06-20 17:20:56 +0100

    fixed duplicate word spellcheck request

--- src/spellcheck.js
+++ src/spellcheck.js
@@ -8,4 +8,34 @@ const spellcheck = axios.create({
   }
 });
 
+function removeLastWordRequest() {
+  if (window.localStorage.getItem('last_word_request')) {
+    window.localStorage.removeItem('last_word_request');
+  }
+}
+removeLastWordRequest();
+
+// spellcheck request interceptor
+spellcheck.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  if (window.localStorage.getItem('last_word_request')) {
+    let lastReqUrl = window.localStorage.getItem('last_word_request');
+    if (lastReqUrl === arguments[0]['url']) {
+      return Promise.reject(false);
+    }
+  }
+
+  window.localStorage.setItem('last_word_request', arguments[0]['url']);
+
+  // remove last word request after 5 seconds
+  setTimeout(() => {
+    removeLastWordRequest();
+  }, 5000);
+
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
 export default spellcheck;
