99be175bff8625ccad23475d18d84fa42b7ad88b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-07-21 16:13:39 +0100

    Created Loading Component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Loading.scss';
+
+const Loading = (props) => (
+  <div className={(props.large ? 'loading' : 'loading loading-lg')}></div>
+);
+
+Loading.propTypes = {
+  large: PropTypes.bool
+};
+
+Loading.defaultProps = {
+  large: false
+};
+
+export default Loading;
