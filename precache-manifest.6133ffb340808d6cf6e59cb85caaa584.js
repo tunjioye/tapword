ce8300db0211eee02f97227ce07add918d87d644
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-07-21 18:08:16 +0100

    Updates

--- precache-manifest.6133ffb340808d6cf6e59cb85caaa584.js
+++ precache-manifest.6133ffb340808d6cf6e59cb85caaa584.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ab2f6f29530e1090a836c07641c21a7",
+    "revision": "4aa5eba3a6a3f9c5221d00bdda061b45",
     "url": "/tapword/index.html"
   },
   {
-    "revision": "b5964b3cf977a3811844",
-    "url": "/tapword/static/css/main.549f10b7.chunk.css"
+    "revision": "ca6a7e8ecd52a3f7d233",
+    "url": "/tapword/static/css/main.ab9a02c7.chunk.css"
   },
   {
     "revision": "c2a86fa06b7d3e8090de",
     "url": "/tapword/static/js/2.852cff39.chunk.js"
   },
   {
-    "revision": "b5964b3cf977a3811844",
-    "url": "/tapword/static/js/main.2f9e64ef.chunk.js"
+    "revision": "ca6a7e8ecd52a3f7d233",
+    "url": "/tapword/static/js/main.2cdeffd7.chunk.js"
   },
   {
     "revision": "7cc087fe62f8accee4dc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tapword/precache-manifest.0c7ab39d4edad860d15f5844f18ec9cb.js"
+  "/tapword/precache-manifest.6133ffb340808d6cf6e59cb85caaa584.js"
 );
 
 self.addEventListener('message', (event) => {
