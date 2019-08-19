5acd5d7c69450fe1448213bddeab24476ac64a7b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-08-19 17:54:08 +0100

    Created Modal Component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Modal.scss'
+
+const Modal = ({ id, noTransparency, canClose, title, children }) => (
+  <div className="modal" id={id}>
+    {noTransparency
+      ? <span className="modal-overlay" style={{ background: 'rgb(25, 28, 34)' }}></span>
+      : <a href="#close" className="modal-overlay" aria-label="Close"> </a>
+    }
+    <div className="modal-container">
+      <div className="modal-header">
+        {canClose &&
+          <a href="#close" className="btn btn-clear" aria-label="Close"> </a>
+        }
+        <div className="modal-title">
+          <strong style={spacedTypo}>{ title }</strong>
+        </div>
+      </div>
+      <div className="modal-body">
+        <div className="content">
+          { children }
+        </div>
+      </div>
+      <div className="modal-footer"></div>
+    </div>
+  </div>
+)
+
+const spacedTypo = {
+  letterSpacing: '0.1rem',
+  wordSpacing: '0.2rem',
+  textTransform: 'uppercase',
+  margin: 0,
+  fontWeight: 600
+}
+
+Modal.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+  id: PropTypes.string,
+  transparency: PropTypes.bool,
+  canClose: PropTypes.bool
+}
+
+Modal.defaultProps = {
+  children: 'Here is the default modal content ...',
+  title: 'TITLE',
+  id: 'modal',
+  noTransparency: false,
+  canClose: true
+}
+
+export default Modal
