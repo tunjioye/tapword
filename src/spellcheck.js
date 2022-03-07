3cfdc92be4f0650359b1cc00cb36013a8ef77c0b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2022-03-08 00:02:52 +0100

    UPDATE : spellcheck api key

--- src/spellcheck.js
+++ src/spellcheck.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import config from './config';
 
 const spellcheck = axios.create({
   baseURL: 'https://montanaflynn-spellcheck.p.rapidapi.com/check/',
   headers: {
-    'X-RapidAPI-Key': config.SPELLCHECK_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_SPELLCHECK_API_KEY,
   }
 });
 
