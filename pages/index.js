32146f33680949edbfd656e5783092030a634c96
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2022-05-29 09:37:21 +0100

    FIX : meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,15 @@ export default function Home() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico?v=0" />
+        <link rel="shortcut icon" href="/favicon.ico?v=0" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#252931" />
 
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicon-16x16.png" />
-        <link rel="mask-icon" href="%PUBLIC_URL%/safari-pinned-tab.svg" color="#334455" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#334455" />
         <meta name="apple-mobile-web-app-title" content="Tapword" />
         <meta name="application-name" content="Tapword" />
         <meta name="msapplication-TileColor" content="#2288ff" />
